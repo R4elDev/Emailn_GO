293dd9b06dfd35608dbae5276ade3b18c1246616
Israel Junior <israel.junior@ndevs.com.br>
2025-07-08 17:39:32 -0300

    Atualizando arquivos de teste, deixando mais legivel

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,21 +1,61 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func TestNewCampaign(t *testing.T) {
-	name := "Campaign x"
-	content := "Body"
-	contacts := []string{"email1@e.com", "email2@e.com"}
+	"github.com/stretchr/testify/assert" // Usando para melhorar a legibilidade do código, retirando ifs
+)
 
+// Constante sendo utilizada no código
+var (
+	name     = "Campaign x"
+	content  = "Body"
+	contacts = []string{"email1@e.com", "email2@e.com"}
+)
+
+/*
+	Para fazer um teste de unidade você precisa ter noção sobre o que é os 3 As:
+	Arrange: Preparar o ambiente para o teste ( Organizar o teste)
+	Act: Executar a ação que está sendo testada
+	Assert: Verificar se o resultado é o esperado
+*/
+
+func TestNewCampaign_CreateCampaign(t *testing.T) {
+
+	// Arrange
+	assert := assert.New(t)
+
+	// Act
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.ID != "1" {
-		t.Errorf("expected 1")
-	} else if campaign.Name != name {
-		t.Errorf("expected correct name")
-	} else if campaign.Content != content {
-		t.Errorf("expected correct content")
-	} else if len(campaign.Contacts) != len(contacts) {
-		t.Errorf("expected correct contacts")
-	}
+	// Assert
+	println(campaign.ID)
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaing_IDIsNotNill(t *testing.T) {
+	// Arrange
+	assert := assert.New(t)
+
+	// Act
+	campaign := NewCampaign(name, content, contacts)
+
+	// Assert
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaing_CreateOnMustBeNow(t *testing.T) {
+	// Arrange
+	assert := assert.New(t)
+
+	now := time.Now().Add(-time.Minute)
+
+	// Act
+	campaign := NewCampaign(name, content, contacts)
+
+	// Assert
+	assert.Greater(campaign.CreatedOn, now) // Lembrar que valores de data sempre vão retornar um valor, ou seja, não valide para que caso ele seja nulo
 }
